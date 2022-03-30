7eb00161e57230ed6e00fcbb914adb8d9510de50
Linda <lindaallinson123@gmail.com>
2022-03-30 18:06:03 +0100

    changed format: user story/action is capitalised eg: BORROW BOOK followed by any description on how to use the code in lower case on the same line which is necessary as some scripts won’t work without editing them first for which we need instructions. For example borrowing a book needs the book_id to update the data so the script using book_id to show where it goes but when actually used you will need to put the actual book id in. However, other scripts for example CHECK IF BOOK IS ON LOAN is just viewing the data and can be used straight away.

--- library_statements.sql
+++ library_statements.sql
@@ -1,12 +1,12 @@
--- check if a book is on loan
+-- CHECK IF A BOOK IS ON LOAN
 use books;
 select book_id from on_loan;
 
--- borrow a book, need to know book_id and replace it in the last line (where id = book_id)
-use loan;
-insert into loan (user_id,	branch_id,	book_id,  date_loaned)  values ( 1, 1, 1, 2022-03-29);
-use books;
-update loan set on_loan = "YES" where id = book_id;
+-- BORROW A BOOK, need to know book_id and replace it in the last line (where id = book_id)
+-- use loan;
+-- insert into loan (user_id,	branch_id,	book_id,  date_loaned)  values ( user_id, branch_id, book_id, 00-00-00);
+-- use books;
+-- update loan set on_loan = "YES" where id = book_id;
 
 
 
