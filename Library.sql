764e743848ca2ea5158e654b7898d231b7ee06b1
Alexandra Morris <alexandramorris@hotmail.com>
2022-03-31 09:41:02 +0100

    added data to tables

--- Library.sql
+++ Library.sql
@@ -105,38 +105,48 @@ foreign key (book_id) references books(book_id),
 author_id int not null,
 foreign key (author_id) references author(author_id));
 
-insert into staff(staff_id, employee_number, staff_name, phone, email, branch_id, job_role, salary, national_insurance)
- values (1, 567, 'Jason', 0786948756, 'js@hotmail.com', 1, 'librarian', 20000, 'ah34567');
-
+insert into staff
+ values (1, 567, 'Jason', 0786948756, 'js@hotmail.com', 1, 'librarian', 20000, 'ah34567'),
+		(2, 852, 'Susan', 0788798756, 'Sue@hotmail.com', 2, 'Manager', 50000, 'js78556'),
+		(3, 789, 'Emma', 0789458714, 'et@gmail.com', 1, 'Librarian', 20000, 'yg781295');
+ 
 insert into address
-values (3, 'London', 34, 'Grey Street', 'Kensington', 'London', 'L2 7HG');
-
-select * from address;
+values (1, 'Leeds', 15, 'Smith Street', 'Headingley', 'Leeds', 'L2 7HG'),
+	   (2, 'Newcastle', 10, 'Happy Street', 'Walker', 'Newcastle', 'NE6 7HG'),
+	   (3, 'London', 34, 'Grey Street', 'Kensington', 'London', 'L2 7HG');
 
 insert into author
 values (1, 'JK', 'Rowling'),
 	   (2, 'Anne', 'Fine'),
        (3, 'James', 'Herbert');
 
-select * from author;
-
 insert into branch
 values(1, 23, 078541254, 'Leeds', 20000),
 	  (2, 123, 075212442, 'Newcastle', 150000),
       (3, 78, 0457812557, 'London', 250000);
 
-select * from branch;
-
 insert into users
-values(1, 547854, 'John Smith', 45, '2022-01-12', '2005-05-10', 5.00, 4.00, 'yes', 'yes');
+values(1, 547854, 'John Smith', 45, '2022-01-12', '2005-05-10', 5.00, 1.00, 'no', 'no'),
+	  (2, 544854, 'Mary White', 28, '2022-01-10', '2018-08-02', 8.00, 1.00, 'no', 'no'),
+	  (3, 687854, 'Sarah Little', 18, '2022-01-15', '2017-01-17', 6.00, 2.00, 'yes', 'yes');
 
-select * from users;
+insert into books
+values(1, 125.789, 'th8795147', 'Harry Potter and the Prisoner of Azkaban', 'JK Rowling', null, 'fantasy', '2000', 1, 2, 'yes'),
+	  (2, 781.985, 'kj7854785', 'Notso Hotso', 'Anne Fine', null, 'childrens', 2001, 1, 3, 'yes'),
+      (3, 699.744, 'yt7125485', 'Ash', 'James Herbert', null, 'horror', 2010, 2, 1, 'no');
+      
+insert into book_author
+values(1, 1, 1),
+	  (2, 2, 2),
+      (3, 3, 3);
 
-insert into users
-values(2, 544854, 'Mary White', 28, '2022-01-10', '2018-08-02', 8.00, 4.50, 'yes', 'yes'),
-	  (3, 687854, 'Sarah Little', 18, '2022-01-15', '2017-01-17', 5.50, 2.00, 'yes', 'yes');
-
-
-    
+insert into loans
+values(1, 1, 1, 1, '2022-01-01', '2022-05-01'),
+	  (2, 2, 2, 2, '2022-02-18', '2022-06-15'),
+      (3, 1, 1, 3, '2021-12-15', '2022-02-14');
+      
+insert into reservations
+values(1, 2, 1, '2022-05-02'),
+	  (2, 1, 2, '2022-02-15');
 
     
